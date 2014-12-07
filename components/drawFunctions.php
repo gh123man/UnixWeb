90935250d0a0477850a38b5afc09809600309fef
PGleo86 <jjh8948@rit.edu>
2014-12-07 18:52:00 -0500

    commit that hopefully doesn't fuck shit up

--- components/drawFunctions.php
+++ components/drawFunctions.php
@@ -40,40 +40,46 @@ function drawLogin() {
 		<div id="loginWrapper" style="float: left;">
 			<form type="post" id="loginForm">
 			<h3>If you are already registered:</h3>
-			<p>
-				<label for="email">Email:</label>
-				<input onkeypress="handleKey(event,this)" type="email" name="username" id="username" style="position: relative; left: 41px;">
-				<br/><br/>
-				<label for="password">Password:</label>
-				<input onkeypress="handleKey(event,this)" type="password" name="password" id="password" style="position: relative; left: 13px;">
+			<table id="loginTable">
+				<tr>
+					<td><label for="email">Email:</label></td>
+					<td><input onkeypress="handleKey(event,this)" type="email" name="username" id="username"></td>
+				</tr>
+				<tr>
+					<td><label for="password">Password:</label></td>
+					<td><input onkeypress="handleKey(event,this)" type="password" name="password" id="password"></td>
+				</tr>
+			</table>
 				<div id="credsOut"></div>
-			</p>
-			<p>
+			
 				<input type="submit" name="submit" id="submit" value="Submit">
 			</p>
 		</form></div>
 		<div id="loginWrapper" style="float: right;">
 		<form type="post" id="registerForm">
 			<h3>Register as a new user:</h3>
-			<p>
-				<label for="textfield">First Name:</label>
-				<input type="text" name="fname" id="fname" style="position: relative; left: 5px;">
-				<br/><br/>
-				<label for="textfield2">Last Name:</label>
-				<input type="text" name="lname" id="lname" style="position: relative; left: 7px;">
-			</p>
-			<p>
-				<label for="email">Email:</label>
-				<input type="email" name="email" id="newEmail" style="position: relative; left: 42px;">
-			</p>
-			<p>
-				<label for="password2">Password:</label>
-				<input type="password" name="password" id="newpassword" style="position: relative; left: 14px;">
-			</p>
-			<p>
-				<label for="password3">Re-Enter Password:</label>
-				<input type="password" name="password1" id="renewpassword" style="position: relative; left: 5px;">
-			</p>
+			<table id="loginTable">
+				<tr>
+					<td><label for="textfield">First Name:</label></td>
+					<td><input type="text" name="fname" id="fname"></td>
+				</tr>
+				<tr>
+					<td><label for="textfield2">Last Name:</label></td>
+					<td><input type="text" name="lname" id="lname"></td>
+				</tr>
+				<tr>
+					<td><label for="email">Email:</label></td>
+					<td><input type="email" name="email" id="newEmail"></td>
+				</tr>
+				<tr>
+					<td><label for="password2">Password:</label></td>
+					<td><input type="password" name="password" id="newpassword"></td>
+				</tr>
+				<tr>
+					<td><label for="password3">Re-Enter Password:</label></td>
+					<td><input type="password" name="password1" id="renewpassword"></td>
+				</tr>
+			</table>
 			<p>
 				<input type="submit" name="submit" id="submit" value="Submit">
 				<div id="regOut"></div>
