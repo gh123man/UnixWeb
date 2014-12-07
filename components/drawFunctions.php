4e1631a7e795a92053d3ed51c094ec6a765a3822
PGleo86 <jjh8948@rit.edu>
2014-12-07 18:13:54 -0500

    Login box alignment

--- components/drawFunctions.php
+++ components/drawFunctions.php
@@ -36,44 +36,51 @@ function drawLogin() {
             ?>
         </div>
     </div>
-    <div id="loginBox"><form type="post" id="loginForm">
-        <h3>If you are already registered:</h3>
-        <p>
-            <label for="email">Email:</label>
-            <input onkeypress="handleKey(event,this)" type="email" name="username" id="username">
-            <label for="password">Password:</label>
-            <input onkeypress="handleKey(event,this)" type="password" name="password" id="password">
-            <div id="credsOut"></div>
-        </p>
-        <p>
-            <input type="submit" name="submit" id="submit" value="Submit">
-        </p>
-    </form>
-    <form type="post" id="registerForm">
-        <h3>Register as a new user:</h3>
-        <p>
-            <label for="textfield">First Name:</label>
-            <input type="text" name="fname" id="fname">
-            <label for="textfield2">Last Name:</label>
-            <input type="text" name="lname" id="lname">
-        </p>
-        <p>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="newEmail">
-        </p>
-        <p>
-            <label for="password2">Password:</label>
-            <input type="password" name="password" id="newpassword">
-        </p>
-        <p>
-            <label for="password3">Re-Enter Password:</label>
-            <input type="password" name="password1" id="renewpassword">
-        </p>
-        <p>
-            <input type="submit" name="submit" id="submit" value="Submit">
-            <div id="regOut"></div>
-        </p>
-    </form></div>
+    <div id="loginBox">
+		<div id="loginWrapper" style="float: left;">
+			<form type="post" id="loginForm">
+			<h3>If you are already registered:</h3>
+			<p>
+				<label for="email">Email:</label>
+				<input onkeypress="handleKey(event,this)" type="email" name="username" id="username" style="position: relative; left: 41px;">
+				<br/><br/>
+				<label for="password">Password:</label>
+				<input onkeypress="handleKey(event,this)" type="password" name="password" id="password" style="position: relative; left: 13px;">
+				<div id="credsOut"></div>
+			</p>
+			<p>
+				<input type="submit" name="submit" id="submit" value="Submit">
+			</p>
+		</form></div>
+		<div id="loginWrapper" style="float: right;">
+		<form type="post" id="registerForm">
+			<h3>Register as a new user:</h3>
+			<p>
+				<label for="textfield">First Name:</label>
+				<input type="text" name="fname" id="fname" style="position: relative; left: 5px;">
+				<br/><br/>
+				<label for="textfield2">Last Name:</label>
+				<input type="text" name="lname" id="lname" style="position: relative; left: 7px;">
+			</p>
+			<p>
+				<label for="email">Email:</label>
+				<input type="email" name="email" id="newEmail" style="position: relative; left: 42px;">
+			</p>
+			<p>
+				<label for="password2">Password:</label>
+				<input type="password" name="password" id="newpassword" style="position: relative; left: 14px;">
+			</p>
+			<p>
+				<label for="password3">Re-Enter Password:</label>
+				<input type="password" name="password1" id="renewpassword" style="position: relative; left: 5px;">
+			</p>
+			<p>
+				<input type="submit" name="submit" id="submit" value="Submit">
+				<div id="regOut"></div>
+			</p>
+			</form>
+		</div>
+	</div>
 
 <?php
 }
