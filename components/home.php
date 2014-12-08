d19a975e55a8b0517cadde5a058f82b8bfbf92c5
Brian Floersch <gh123man@gmail.com>
2014-12-07 20:56:24 -0500

    fixed homepage

--- components/home.php
+++ components/home.php
@@ -10,11 +10,13 @@ echo '</div>';
 <div class="pageContent pannel pageContentHomePage">
     <div id="main">
         <img src="/static/images/homepage.png">
-        <p>UnixWeb is a place for anyone to learn and reference everything Unix. If you want to look up examples of commands
-            as well as explanations, you can view them under docs. UnixWeb provides a variety of tutorials and quizzes to expand
-            and test your knowledge. Head on over to the History to get a glimspe how unix was developed throughout time.
-        </p>
     </div>
+    
+    <p class="homeContent">UnixWeb is a place for anyone to learn and reference everything Unix. If you want to look up examples of commands
+        as well as explanations, you can view them under docs. UnixWeb provides a variety of tutorials and quizzes to expand
+        and test your knowledge. Head on over to the History to get a glimspe how unix was developed throughout time.
+    </p>
+
 
     <!--generate command of the day / top 10 with javascript queries-->
     <div id="command-of-day">
